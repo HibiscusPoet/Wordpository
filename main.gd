608ec7cb681495da307afc86eaf9853098d038a6
HibiscusPoet <86578158+HibiscusPoet@users.noreply.github.com>
2023-06-06 19:41:27 -0700

    Final Commit

--- main.gd
+++ main.gd
@@ -9,8 +9,11 @@ extends Control
 #import Engine
 
 @onready var textEdit = $TextEdit
+@onready var progressBar = $HBoxContainer2/ProgressBar
+
 
 var text
+var goal = 1
 
 func _unhandled_input(event):
 	if Input.is_action_just_pressed("save"):
@@ -32,3 +35,21 @@ func load():
 	file.close()
 	
 	return textInFile
+
+func CountWordInString(string:String):
+	var wordTotal = -1
+	var previousSpace = 0
+	
+	while (previousSpace != -1):
+		previousSpace = string.find(" ", previousSpace + 1)
+		wordTotal += 1
+	
+	return wordTotal
+
+func _on_text_edit_text_changed():
+	var currentWord = CountWordInString($TextEdit.text)
+	progressBar.value = (currentWord / goal) * 100
+	#divide small number by big number and multiply by 100
+
+func _on_spin_box_value_changed(value):
+	goal = value
